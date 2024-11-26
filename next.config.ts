9cc9ab6352567eba0d628ed6bcc7a61cda314d62
youneedgreg <gregorytemwa1212@gmail.com>
2024-11-26 15:04:38 +0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     domains: ['sevendaysinn.co.ke'], // Add your domain here
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

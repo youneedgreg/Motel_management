a1c5525d3543824a6ca6647ab8f08a221a8cff4f
youneedgreg <gregorytemwa1212@gmail.com>
2024-11-26 16:12:33 +0300

    didnt commit

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,45 +1,42 @@
-import { NextResponse } from 'next/server'
-import { prisma } from '@/lib/prisma'
-import bcrypt from 'bcryptjs'
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
 
 export async function POST(request: Request) {
   try {
-    const body = await request.json()
-    const { empId, password } = body
+    const body = await request.json();
+    const { empId, password } = body;
 
     const user = await prisma.user.findUnique({
       where: {
         empId,
       },
-    })
+    });
 
     if (!user) {
       return NextResponse.json(
         { error: 'User not found' },
         { status: 404 }
-      )
+      );
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password)
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
       return NextResponse.json(
         { error: 'Invalid password' },
         { status: 401 }
-      )
+      );
     }
 
-    const result = { ...user };
-    delete result.password;
+    // Create a new object excluding the password property
+    const { password: _, ...result } = user;
 
-
-    return NextResponse.json(result)
-} catch (error) {
+    return NextResponse.json(result);
+  } catch (error) {
     console.error('User log in error:', error);
-    return new Response(JSON.stringify({ error: 'Error creating user' }), {
+    return new Response(JSON.stringify({ error: 'Error during user login' }), {
       status: 500,
     });
   }
-  
-
 }
